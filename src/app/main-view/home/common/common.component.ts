0c5da8e1b4b6cb947dda63a1bb66312857cbd804
nuhatareq <nohanuha8@gmail.com>
2024-09-03 10:13:00 +0200

    - common section works with its responsive

--- src/app/main-view/home/common/common.component.ts
+++ src/app/main-view/home/common/common.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CardHomeComponent } from '../../../shared-components/card-home/card-home.component';
 
 @Component({
   selector: 'app-common',
   standalone: true,
-  imports: [],
+  imports: [CardHomeComponent],
   templateUrl: './common.component.html',
-  styleUrl: './common.component.css'
+  styleUrl: './common.component.css',
 })
 export class CommonComponent {
-
+  links: Array<string> = [
+    'خدمات الاحوال المدنية',
+    'خدمات المرور',
+    'خدمات الحج والعمرة',
+    'خدمات الادلة الجنائية',
+    'خدمات تصاريح العمل',
+  ];
 }
